0b372bb8eacf7984d23d0e0cbf8fe7e8b2e8431e
Takypirina <antonmediaproductions@gmail.com>
2025-12-01 07:50:19 -0800

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", () => {
-  // Service worker minimale: pronto per futuri usi offline.
-});
-
-self.addEventListener("fetch", () => {
-  // Per ora non intercetta nulla; lascia passare tutte le richieste.
-});
